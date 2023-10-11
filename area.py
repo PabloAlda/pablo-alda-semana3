be71e008735169fdd376fdd7ee1ac5943bef05b3
PabloAlda <pabloam0309@gmail.com>
2023-10-11 21:40:00 +0200

    Update area.py

--- area.py
+++ area.py
@@ -6,8 +6,10 @@ def area_pentagono (lado,apotema):
 #Pido apotema y lado al usuario
 lado_input = float(input("Longitud de los lados"))
 apotema_input = float(input("Longitud apotema"))
- 
+
+#Guardo el valor del area calculada
 area_calculada = area_pentagono(lado_input,apotema_input)
 
+#Muestro en pantalla el resultado del area
 print(area_calculada)
 
